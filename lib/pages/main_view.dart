a1e89ae9a6af30231e3441879bf7812f749b425b
Rümeysa Yavuzkanat <118269494+rumeysayk@users.noreply.github.com>
2025-07-25 17:26:01 +0300

    Added some pages.

---
+++ lib/pages/main_view.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:awesome_bottom_bar/awesome_bottom_bar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../constants/colors.dart';
+
+import 'home_page.dart';
+import 'practice_page.dart';
+import 'simulation_page.dart';
+import 'library_page.dart';
+import 'profile_page.dart';
+
+class MainView extends StatefulWidget {
+  const MainView({super.key});
+
+  @override
+  State<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = const [
+    HomePage(),
+    PracticePage(),
+    SimulationPage(),
+    LibraryPage(),
+    ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: _screens[_currentIndex],
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 24.0, left: 16.0, right: 16.0),
+        child: BottomBarFloating(
+          items: const [
+            TabItem(icon: Icons.home, title: 'Home'),
+            TabItem(icon: FontAwesomeIcons.penToSquare, title: 'Practice'),
+            TabItem(icon: FontAwesomeIcons.stopwatch, title: 'Simulate'),
+            TabItem(icon: FontAwesomeIcons.bookOpen, title: 'Library'),
+            TabItem(icon: FontAwesomeIcons.user, title: 'Profile'),
+          ],
+          backgroundColor: prussianBlue,
+          color: ivory,
+          colorSelected: ivory,
+          borderRadius: BorderRadius.circular(50),
+          indexSelected: _currentIndex,
+          iconSize: 20,
+          paddingVertical: 18,
+          onTap: (int index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
+/*
+import 'package:flutter/material.dart';
+import 'package:awesome_bottom_bar/awesome_bottom_bar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../constants/colors.dart';
+
+import 'home_page.dart';
+import 'practice_page.dart';
+import 'simulation_page.dart';
+import 'library_page.dart';
+import 'profile_page.dart';
+
+class MainView extends StatefulWidget {
+  const MainView({super.key});
+
+  @override
+  State<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = const [
+    HomePage(),
+    PracticePage(),
+    SimulationPage(),
+    LibraryPage(),
+    ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: prussionBlue,
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomBarDefault(
+        items: const [
+          TabItem(icon: Icons.home, title: 'Home'),
+          TabItem(icon: FontAwesomeIcons.penToSquare, title: 'Practice'),
+          TabItem(icon: FontAwesomeIcons.stopwatch, title: 'Simulate'),
+          TabItem(icon: FontAwesomeIcons.bookOpen, title: 'Library'),
+          TabItem(icon: FontAwesomeIcons.user, title: 'Profile'),
+        ],
+        backgroundColor: prussionBlue,
+        color: ivory,
+        colorSelected: prussionBlue,
+        indexSelected: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
+
+
+ */
---
+++ lib/pages/simulation_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SimulationPage extends StatelessWidget {
+  const SimulationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(child: Text('Simulation Page')),
+    );
+  }
+}
